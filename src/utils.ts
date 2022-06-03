1648e8a61049362aa59d059011049e9bd1ec148b
glaiveeeeeee <93272060+glaiveeeeeee@users.noreply.github.com>
2022-06-03 22:32:33 +0000

    oops

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { fileURLToPath } from 'node:url'
-import readdirp from 'readdirp'
-
-export function crawlEvents () {
-  readdirp(fileURLToPath(new URL('../events', import.meta.url)), {
-    fileFilter: '*.js'
-  })
-}
